ac3ed471da700b542055b8abdcd539944cd02957
Thomas Levine <perluette@thomaslevine.com>
2012-05-17 12:26:25 -0400

    hipstamatic

---
+++ hipstamatic.r
@@ -0,0 +1,17 @@
+hipstamatic <- function(...){
+  print('I want this to be a device that plots to png and then hipstamatizes')
+}
+
+hipstogram <- function(x,
+  filename='hipstogram.png', width=1024, height=800,
+  ...
+) {
+  png(filename='.histogram.png', width=width, height=height)
+  hist(x, ...)
+  dev.off()
+
+  # Pass through the Hipstomatic
+  # https://github.com/paulasmuth/hipster_filters/blob/master/vintage_processor.rb
+  outputoptions <- '-compose over -gravity center -modulate 100,120 -brightness-contrast 0x20'
+  system(paste('convert','.histogram.png',outputoptions,filename))
+}
